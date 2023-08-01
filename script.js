bfa9447333fc02c646a3d1abc1506a55c9a547c1
Brian <wamwangimathenge@gmail.com>
2023-08-01 15:04:45 +0300

    Write a first class function
    This commit demonstrates a first-class function that can pass a
    function invocation to another function

--- script.js
+++ script.js
@@ -27,3 +27,26 @@ var virtualPet = {
     }
 }
 console.log(virtualPet.sleepy)
+
+
+// FIrst Class Functions
+function addTwoNums(a, b) {
+    console.log(a + b)
+}
+
+function randomNum() {
+    return Math.floor((Math.random() * 10) + 1);
+}
+function specificNum() { return 42 };
+
+var useRandom = true;
+
+var getNumber;
+
+if(useRandom) {
+    getNumber = randomNum
+} else {
+    getNumber = specificNum
+}
+
+addTwoNums(getNumber(), getNumber())
