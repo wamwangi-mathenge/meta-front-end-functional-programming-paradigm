a01c75d8e2651e67dc931a178686e33465115f2a
Brian <wamwangimathenge@gmail.com>
2023-08-01 14:47:03 +0300

    Demonstrate the Functional Programming Paradigm
    This commit writes 2 functions that demonstrate the functional
    programming paradigm in which data and functionality are kept separate
    and data is passed into functions only when something is to be
    computed.

---
+++ script.js
@@ -0,0 +1,20 @@
+// Functional Programming
+
+function getTotal(a, b) {
+    return a + b
+}
+var num1 = 2;
+var num2 = 3;
+
+var total = getTotal(num1, num2);
+
+console.log(total)
+
+function getDistance(mph, h) {
+    return mph * h
+}
+var mph = 60;
+var h = 2;
+var distance = getDistance(mph, h)
+
+console.log(distance)
